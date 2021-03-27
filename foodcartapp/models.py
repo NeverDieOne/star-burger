1a9f629d666207c55441daa34d249bcf8e1e514a
neverdieone <altiore@mail.ru>
2021-03-27 19:09:41 +0300

    Add restaurants to order

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -73,13 +73,35 @@ class RestaurantMenuItem(models.Model):
 
 
 class OrderQuerySet(models.QuerySet):
-    def fetch_with_total_price(self):
-        return self.annotate(
-            total_price=Sum(
-                'order_items__price',
-                output_field=models.DecimalField()
-            ),
-        )
+    def fetch_with_restaurant(self):
+        menu_items = RestaurantMenuItem.objects.select_related('restaurant', 'product') \
+            .prefetch_related('product__order_items__order')
+
+        orders = set()
+        handled_order_items = set()
+        for menu_item in menu_items:
+            if not menu_item.availability:
+                continue
+            for order_item in menu_item.product.order_items.all():
+                try:
+                    order_item.order.restaurants
+                except AttributeError:
+                    order_item.order.restaurants = []
+                    order_item.order.total_price = 0
+
+                orders.add(order_item.order)
+
+                restaurant_name = menu_item.restaurant.name
+                if restaurant_name not in order_item.order.restaurants:
+                    order_item.order.restaurants.append(restaurant_name)
+
+                if order_item not in handled_order_items:
+                    order_item.order.total_price += order_item.price
+                    handled_order_items.add(order_item)
+
+        sorted_orders = sorted(list(orders), key=lambda order: order.id)
+        return sorted_orders
+
 
 class Order(models.Model):
     UNPROCESSED = 'Unproc'
